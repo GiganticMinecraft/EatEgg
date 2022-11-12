96786d276965baeb870bca005fdcf32447e1e82e
Lucky3028 <Lucky3028@users.noreply.github.com>
2022-11-12 11:29:37 +0900

    fix: the value in config may be null

--- src/main/scala/click/seichi/eategg/EnabledWorlds.scala
+++ src/main/scala/click/seichi/eategg/EnabledWorlds.scala
@@ -3,6 +3,7 @@ package click.seichi.eategg
 import org.bukkit.plugin.java.JavaPlugin
 import scala.jdk.CollectionConverters._
 import scala.collection.mutable
+import java.util.Collections
 
 object EnabledWorlds {
   private final val ConfigPath = "enabled-worlds"
@@ -11,7 +12,11 @@ object EnabledWorlds {
   def load()(implicit instance: JavaPlugin): Unit = {
     worlds.clear()
 
-    instance.getConfig.getStringList(ConfigPath).asScala.foreach(worlds.add)
+    Option(instance.getConfig.getStringList(ConfigPath))
+      .getOrElse(Collections.emptyList())
+      .asScala
+      .map(_.toLowerCase)
+      .foreach(worlds.add)
   }
 
   def get(): Set[String] = worlds.toSet
