4a2e3a2e9628dc4fec978a5d21e4a9cdb6e4501f
Lucky3028 <Lucky3028@users.noreply.github.com>
2022-11-13 19:05:12 +0900

    fix: reload config from console
    close #4

--- src/main/scala/click/seichi/eategg/commands/Reload.scala
+++ src/main/scala/click/seichi/eategg/commands/Reload.scala
@@ -2,15 +2,19 @@ package click.seichi.eategg.commands
 
 import click.seichi.eategg.EnabledWorlds
 import org.bukkit.ChatColor
+import org.bukkit.entity.Player
 import org.bukkit.plugin.java.JavaPlugin
 
 case class Reload()(implicit plugin: JavaPlugin) extends Executor {
   override val commandName: String = "reload"
 
   override def execute(context: CommandContext): Unit = {
-    if (!context.sender.hasPermission(s"eategg.$commandName")) {
-      context.sender.sendMessage(s"${ChatColor.RED}このコマンドを実行する権限がありません。")
-      return
+    val perm = s"eategg.$commandName"
+    context.sender match {
+      case player: Player if !player.hasPermission(perm) =>
+        player.sendMessage(s"${ChatColor.RED}このコマンドを実行する権限がありません。: $perm")
+        return
+      case _ =>
     }
 
     EnabledWorlds.load()
